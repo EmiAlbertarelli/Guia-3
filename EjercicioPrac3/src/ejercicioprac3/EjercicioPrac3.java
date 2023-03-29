5ef935e7343c6ad7c1bfc7c28766dd281f8c016a
Emi Albertrelli <emialbertarelli@hotmail.com>
2023-03-29 19:55:49 -0300

    https://github.com/EmiAlbertarelli/Guia-3.git

--- EjercicioPrac3/src/ejercicioprac3/EjercicioPrac3.java
+++ EjercicioPrac3/src/ejercicioprac3/EjercicioPrac3.java
@@ -2,16 +2,29 @@
  Realizar un programa que solo permita introducir solo frases o palabras de 8 de largo. 
 Si el usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje
 por pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. 
-Nota: investigar la función Lenght() en Java.
+Nota: investigar la funcion Lenght() en Java.
  */
 package ejercicioprac3;
+
+import java.util.Scanner;
 
 
 public class EjercicioPrac3 {
 
     
     public static void main(String[] args) {
-        Scanner
+        Scanner leer = new Scanner(System.in);
+        String frase;
+        
+        System.out.println("Ingrese una palabra o frase");
+        frase = leer.nextLine();
+        
+        if(frase.length()== 8){
+            System.out.println("Correcto");
+        }else {
+            System.out.println("Incorrecto");
+        }
+        
     }
     
 }
